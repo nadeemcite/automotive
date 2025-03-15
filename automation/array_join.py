61add0a9e6abf228f211112d8f4765429be9b33c
nadeemcite <nady5730642@gmail.com>
2025-03-16 00:48:52 +0700

    fix: minor

--- automation/array_join.py
+++ automation/array_join.py
@@ -6,6 +6,9 @@ from automation.automation_executor import AutomationExecutor
 class ArrayJoin(AutomationExecutor):
 
     def exec(self):
+        """
+        Join the elements of the array into a single string using the specified separator.
+        """
         self.input_variables[self.config["result_var"]] = json.dumps(
             self.config["join_key"].join(json.loads(self.config["source"]))
         )
