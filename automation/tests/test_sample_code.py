3bfc06f5091a8b75de9d3fb358e4ad76b989031e
nadeemcite <nady5730642@gmail.com>
2025-03-16 12:40:05 +0700

    fix: minor

--- automation/tests/test_sample_code.py
+++ automation/tests/test_sample_code.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from automation.sample_code import sample_code
+from automation.sample_code import sum
 
 class TestSampleCode(TestCase):
     def test_sample_code(self):
-        self.assertTrue(sample_code(4, 5), 9)
+        self.assertEqual(sum(4, 5), 9)
