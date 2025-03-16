b958c488479c6b7f5c2be853ed76ca05a9ce50ad
nadeemcite <nady5730642@gmail.com>
2025-03-16 12:44:01 +0700

    fix: minor

--- automation/sample_code.py
+++ automation/sample_code.py
@@ -1,4 +1,4 @@
-def sum(a: int, b: int) -> int:
+def add_two_numbers(a: int, b: int) -> int:
     if a < 0 or b < 0:
         raise ValueError("Only positive integers are allowed")
     return a + b
--- automation/tests/test_sample_code.py
+++ automation/tests/test_sample_code.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from automation.sample_code import sum
+from automation.sample_code import add_two_numbers
 
 class TestSampleCode(TestCase):
     def test_sample_code(self):
-        self.assertEqual(sum(4, 5), 9)
+        self.assertEqual(add_two_numbers(4, 5), 9)
